3207a89659c7ecff7b05c6039ac39f46df883429
gokuludt <gokuludt@gmail.com>
2020-12-07 10:41:21 +0530

    Update SearchText.java
    fixed to support search text at end of the sentence

--- SearchText.java
+++ SearchText.java
@@ -6,26 +6,26 @@ import java.util.Scanner;
  */
 public class SearchText {
 
-	
 	/**
-	 * @param Search Content and Search Key
+	 * @param Search
+	 *            Content and Search Key
 	 */
 	public static void main(String args[]) {
-		
-		Scanner input= new Scanner(System.in); 
-		System.out.print("Enter Search Content: ");  
-		String searchContent= input.nextLine();
-		System.out.print("Enter Search Term: ");  
-		String searchKey= input.nextLine();
-		searchContent=searchContent.toLowerCase();
-		searchKey=searchKey.toLowerCase();
 
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter Search Content: ");
+		String searchContent = input.nextLine();
+		System.out.print("Enter Search Term: ");
+		String searchKey = input.nextLine();
+		searchContent = searchContent.toLowerCase();
+		searchKey = searchKey.toLowerCase();
 
 		int searchKeylength = searchKey.length();
 		int count = 0;
-		
+
 		/**
-		 * @param this block will spilt the search content by word and count it
+		 * @param this
+		 *            block will spilt the search content by word and count it
 		 */
 		String splitContent[] = searchContent.split("\\s+");
 		for (int i = 0; i < splitContent.length; i++) {
@@ -33,24 +33,31 @@ public class SearchText {
 				count++;
 			}
 		}
-		
+
 		/**
-		 * @param this block will verify search content by char and count it
+		 * @param this
+		 *            block will verify search content by char and count it
 		 */
 		if (count == 0) {
 			int wordfirstIdx = searchContent.indexOf(searchKey);
 			int wordlastIdx = searchContent.lastIndexOf(searchKey);
-			String tempsearchContent=searchContent;
+			String tempsearchContent = searchContent;
 			while (wordlastIdx > 0) {
-				if (tempsearchContent.contains(searchKey))
-					count++;
-				wordfirstIdx = tempsearchContent.indexOf(searchKey);
-				wordlastIdx = tempsearchContent.lastIndexOf(searchKey);
-				if (wordfirstIdx == 0) {
-					wordfirstIdx = tempsearchContent.indexOf(searchKey, wordfirstIdx);
-					wordlastIdx = tempsearchContent.lastIndexOf(searchKey, wordlastIdx);
+				if (tempsearchContent.equals("")) {
+					break;
 				}
-				tempsearchContent = tempsearchContent.substring(wordfirstIdx + searchKeylength);
+				if (tempsearchContent.contains(searchKey)) {
+					count++;
+					wordfirstIdx = tempsearchContent.indexOf(searchKey);
+					wordlastIdx = tempsearchContent.lastIndexOf(searchKey);
+					if (wordfirstIdx == 0) {
+						wordfirstIdx = tempsearchContent.indexOf(searchKey, wordfirstIdx);
+						wordlastIdx = tempsearchContent.lastIndexOf(searchKey, wordlastIdx);
+					}
+					tempsearchContent = tempsearchContent.substring(wordfirstIdx + searchKeylength);
+				} else {
+					break;
+				}
 			}
 		}
 		System.out.println("************************************************************************************");
